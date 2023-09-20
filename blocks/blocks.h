f5211083926c40663151b549e8fd9b3445c42400
Michael A Gallo <michaelgallo@protonmail.com>
2023-09-19 23:20:23 -0400

    brought blocks in line with dwmblocks

--- blocks/blocks.h
+++ blocks/blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "memory",					10,		4},
+	{"", "volume",					0,		10},
+	// {"", "battery",					20,		3},
+	{"🕒", "clock",					5,		0}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
